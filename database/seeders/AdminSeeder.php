2e1dd1432e6b171600a0bb38db7b0ab6274103cb
youssraalali <youssraalali4@gmail.com>
2026-05-25 02:14:26 +0300

    Fix AdminSeeder duplicate email on redeploy

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -12,12 +12,14 @@ class AdminSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        User::create([
+{
+    User::firstOrCreate(
+        ['email' => 'admin@gmail.com'],
+        [
             'name' => 'Admin',
-            'email' => 'admin@gmail.com',
             'password' => bcrypt('password'),
             'role' => 'admin',
-        ]);
-    }
+        ]
+    );
+}
 }
